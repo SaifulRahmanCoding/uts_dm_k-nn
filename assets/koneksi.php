f53d0cea540db15a111ca6616e8540b7f700920c
Saiful Rahman <sr.perante@gmail.com>
2022-07-05 09:26:26 +0700

    new

--- assets/koneksi.php
+++ assets/koneksi.php
@@ -2,6 +2,9 @@
 // kalau pakai offline
 $db = new mysqli("localhost","root","","db_knn");
 
+// online
+// $db = new mysqli("localhost","id19218370_db_knn","dm_knn123Rahman","id19218370_dm_knn");
+
 // cek koneksi
 if ($db->connect_error) {
 	echo "Gagal menyambungkan ke MySQL : ".$db->connect_error;
--- config/script.php
+++ config/script.php
@@ -1,3 +1,2 @@
 <script type="text/javascript" src="assets/bootstrap/js/bootstrap.bundle.js"></script>
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
-<!-- <script type="text/javascript" src="assets/js/script.js"></script> -->
